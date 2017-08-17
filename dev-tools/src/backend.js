486059474b7dade9972be04e791bdbdf0e10b6a2
Luobata <602975805@qq.com>
2017-08-17 23:31:40 +0800

    init;
    Signed-off-by: Luobata <602975805@qq.com>

--- dev-tools/src/backend.js
+++ dev-tools/src/backend.js
@@ -7,26 +7,27 @@ window.addEventListener('message', handshake);
 
 function handshake (e) {
     if (e.data.source === 'match-devtools-proxy' && e.data.payload === 'init') {
-        window.removeEventListener('message', handshake)
+        window.removeEventListener('message', handshake);
 
-            let listeners = []
-            const bridge = new Bridge({
-                listen (fn) {
-                    var listener = evt => {
-                        if (evt.data.source === 'match-devtools-proxy' && evt.data.payload) {
-                            fn(evt.data.payload)
-                        }
+        let listeners = [];
+        const bridge = new Bridge({
+            listen (fn) {
+                var listener = evt => {
+                    console.log(evt);
+                    if (evt.data.source === 'match-devtools-proxy' && evt.data.payload) {
+                        fn(evt.data.payload)
                     }
-                    window.addEventListener('message', listener)
-                        listeners.push(listener)
-                },
-                send (data) {
-                    window.postMessage({
-                        source: 'match-devtools-backend',
-                        payload: data
-                    }, '*')
                 }
-            })
+                window.addEventListener('message', listener);
+                listeners.push(listener);
+            },
+            send (data) {
+                window.postMessage({
+                    source: 'match-devtools-backend',
+                    payload: data
+                }, '*')
+            }
+        });
 
         bridge.on('shutdown', () => {
             listeners.forEach(l => {
--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,6 @@
 /**
  * @description 创建面板时触发 绑定hook的刷新事件
+ * @description 在userDebugger 中执行，可以获得window下的内容
  */
 
 const hook = window.__MATCH_DEVTOOLS_GLOBAL_HOOK__;
