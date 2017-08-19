3b19c6f7c4906ae616911ab99bcc509ca34e3b77
Luobata <602975805@qq.com>
2017-08-19 17:29:42 +0800

    add: 过滤区域加入自定义滚动条样式;
    Signed-off-by: Luobata <602975805@qq.com>

--- src/components/matchFilter/matchFilter.vue
+++ src/components/matchFilter/matchFilter.vue
@@ -6,12 +6,14 @@
                 <input placeholder="Filter components" @input="filterStacks">
             </div>
         </action-header>
+        <div class="match-arr-content">
         <div class="matchArrs">
             <ul class="arr-content">
                 <li :class="['arr-items', {'active': activeItem === item}]" v-for="item in matchArr" @click="chooseItem(item)">
                     {{item.url}}
                 </li>
             </ul>
+        </div>
         </div>
     </div>
 </template>
